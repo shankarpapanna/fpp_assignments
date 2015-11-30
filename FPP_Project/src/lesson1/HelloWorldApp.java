3b7c2809816194de3e0fb2c01e59ed1328808501
984882 <984882@Acer-PC-115.cs.mum.edu>
2015-11-30 14:31:40 -0600

    second commit

---
+++ FPP_Project/src/lesson1/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+/**
+ * 
+ * @author 984882
+ *
+ */
+public class HelloWorldApp {
+
+	public static void main(String ar[]){
+		System.out.println("Hello World");
+	}
+}
---
+++ FPP_Project/src/lesson2/CommandLineArg.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class CommandLineArg {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < args.length; ++i) {  
+			System.out.println("position " + i + ": " + args[i]);
+} 
+	}
+         
+}
